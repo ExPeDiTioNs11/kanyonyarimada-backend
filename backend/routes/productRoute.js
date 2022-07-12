fff75ce326743549742a93f529904dfa498bcbf6
NOVACY TECH <49060257+ExPeDiTioNs11@users.noreply.github.com>
2022-07-12 16:36:03 +0300

    add gey by ıd router
    add gey by ıd router

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const { get_product, addNewProduct, update_selected_product, delete_selected_product, getByBarcode} = require('../controllers/productController');
+const { get_product, addNewProduct, update_selected_product, delete_selected_product, getByBarcode, getById} = require('../controllers/productController');
 
 const router = express.Router();
 const { userControl } = require('../middlewares/authMiddleware');
 
 // zincirleme route oluşturma
 router.route('/').get(get_product).post(addNewProduct); 
-router.route('/:id').put(update_selected_product).delete(delete_selected_product);
+router.route('/:id').put(update_selected_product).delete(delete_selected_product).get(getById);
 router.route('/:barcode').get(getByBarcode);
-
 
 module.exports = router;
