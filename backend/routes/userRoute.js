94d452cc280566f33506316fafeb6dd502f0235b
NOVACY TECH <49060257+ExPeDiTioNs11@users.noreply.github.com>
2022-07-12 14:47:35 +0300

    add user delete func
    add user delete func

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { user_register, user_login, get_user, get_myInfo } = require('../controllers/userController');
+const { user_register, user_login, get_user, get_myInfo, delete_selected_user } = require('../controllers/userController');
 const { userControl } = require('../middlewares/authMiddleware')
 const router = express.Router();
 
 router.post('/register', user_register);
 router.post('/login', user_login);
 router.get('/getUser', get_user);
-router.get('/getmyInfo', get_myInfo)
+router.get('/getmyInfo', get_myInfo);
+router.route('/:id').delete(delete_selected_user);
 
 module.exports = router
