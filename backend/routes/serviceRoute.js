d39e85f24813eae9cec2672382bbc60c12031f97
NOVACY TECH <49060257+ExPeDiTioNs11@users.noreply.github.com>
2022-07-17 00:59:30 +0300

    example fix
    example fix

--- backend/routes/serviceRoute.js
+++ backend/routes/serviceRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getService, add_new_service, update_service, deleteService, getByIdService } = require('../controllers/serviceController');
+const { getService, add_new_service, update_service, deleteService } = require('../controllers/serviceController');
 const { userControl } = require('../middlewares/authMiddleware')
 const router = express.Router();
 
 // zincirleme route oluşturma
 router.route('/').get(getService).post(add_new_service); 
-router.route('/:id').put(update_service).delete(deleteService).get(getByIdService);
+router.route('/:id').put(update_service).delete(deleteService);
 
 
 module.exports = router
