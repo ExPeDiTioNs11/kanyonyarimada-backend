dd329406469e3ec792bd3fec6e215c4da8a719f9
NOVACY TECH <49060257+ExPeDiTioNs11@users.noreply.github.com>
2022-07-17 02:02:31 +0300

    ASDASD
    ASDASD

--- backend/routes/customerRoute.js
+++ backend/routes/customerRoute.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const { get_allCustomers, add_new_customer, update_customer, delete_customer} = require('../controllers/customerController');
+const { get_allCustomers, add_new_customer} = require('../controllers/customerController');
 
 const router = express.Router()
 const { userControl } = require('../middlewares/authMiddleware')
 
 router.route('/').get(get_allCustomers).post(add_new_customer)
-router.route('/:id').put(update_customer).delete(delete_customer);
 
 
 module.exports = router;
