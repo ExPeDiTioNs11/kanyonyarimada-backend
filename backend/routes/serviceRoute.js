480fc482ee3d164b9924f46bea97b58a7c85021d
NOVACY TECH <49060257+ExPeDiTioNs11@users.noreply.github.com>
2022-07-17 00:51:00 +0300

    added get by id
    added get by id

--- backend/routes/serviceRoute.js
+++ backend/routes/serviceRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getService, add_new_service, update_service, deleteService } = require('../controllers/serviceController');
+const { getService, add_new_service, update_service, deleteService, getById } = require('../controllers/serviceController');
 const { userControl } = require('../middlewares/authMiddleware')
 const router = express.Router();
 
 // zincirleme route oluşturma
 router.route('/').get(getService).post(add_new_service); 
-router.route('/:id').put(update_service).delete(deleteService);
+router.route('/:id').put(update_service).delete(deleteService).get(getById);
 
 
 module.exports = router
